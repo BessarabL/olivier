4919ce767d95a22fcc50a2c675eba28902c7aaf6
BessarabL <bess.lm.fd@gmail.com>
2024-01-03 15:49:36 +0300

    fix: router fixed with adding outlet in app

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app/App.tsx'
 import './index.scss'
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+import MainRoutes from "./mainRoutes";
+import {
+  RouterProvider,
+} from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <RouterProvider router={MainRoutes} />
+    </Provider>
   </React.StrictMode>,
 )
